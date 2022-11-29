c568e315b7a64c3d56ce22fa16499a9a3778edce
Raionara <raionara.dantas@gmail.com>
2022-11-29 16:21:08 -0300

    algoritmo

---
+++ 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/conteudo.js
@@ -0,0 +1,25 @@
+
+// Algoritmos:
+
+let fruits = [3, 4, 10, 1, 12];
+
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+    sum += fruits[index]; 
+}
+
+if (sum > 15) {
+    console.log(sum);
+} else {
+     console.log('valor menor que 16');
+}
+
+// // Questão 4 - fixando aprendizado:
+// // const n = 9;
+
+// // let resultado = 100;
+// // for (let index = 0; index <= n; index += 1) {
+// //   resultado -= index;
+// // };
+// // console.log(resultado);
