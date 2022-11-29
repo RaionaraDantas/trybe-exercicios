dec6bf7060b523c4c31ef314e6a21c397a9b568e
Raionara <raionara.dantas@gmail.com>
2022-11-29 08:08:16 -0300

    atualização

---
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicicio.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console table
+// numbers é o array
+
+for (let xablau = 0; index < numbers.length; xablau +=1) {
+console.log(numbers[xablau]);
+}
---
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/for.js
@@ -0,0 +1,30 @@
+// var numero = 9;
+// for (var contador = 1; contador <= 9; contador ++) {
+    // console.log(numero * contador);
+// }
+    // repetir do num 1 ao num 9. O resultado será seu proprio valor + 1. Abaixo fica o código que vai se repetir
+
+// For e array:
+
+    // var listaDeNomes = ["Joana", "Maria", "Lucas"];
+    // for (var indice = 0; indice < listaDeNomes.length; indice++) {
+    // var mensagem = "Boas vindas," + listaDeNomes [indice] + "!";
+    // console.log(mensagem)
+    // }
+    // em arreys começa contar do 0. No seg topico n é <= pq o último é o tam -1. ++: +1
+
+    // let cars = ['Saab', 'Volvo', 'BMW'];
+    
+    // console.log(cars[0]); // Saab
+    // console.log(cars[1]); // Volvo
+    // console.log(cars[2]); // BMW
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
